51b7c115d247272e25e00f13e4609255a2dc1f75
bofletcher <bfletcher@blackairplane.com>
2024-12-31 11:13:03 -0500

    Initial commit.

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    //return ['foo' => 'bar'];
+    return view('about')
+});
